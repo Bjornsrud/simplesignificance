09ce34d3e7d0760a772a6cbf6b9793981cc77654
Bjornsrud <ckb78@outlook.com>
2025-04-12 22:10:20 +0200

    Add AnalysisResult.java

---
+++ src/main/java/com/simplesignificance/model/analysis/AnalysisResult.java
@@ -0,0 +1,52 @@
+package com.simplesignificance.model.analysis;
+
+import java.util.List;
+import java.util.Map;
+
+public class AnalysisResult {
+
+    private Map<String, Integer> groupSizes;
+    private Map<String, Double> variances;
+    private Map<String, Boolean> isNormal;
+    private List<TestRecommendation> recommendations;
+    private boolean tooFewDataPoints;
+    private boolean lowPowerWarning;
+
+    public AnalysisResult(Map<String, Integer> groupSizes,
+                          Map<String, Double> variances,
+                          Map<String, Boolean> isNormal,
+                          List<TestRecommendation> recommendations,
+                          boolean tooFewDataPoints,
+                          boolean lowPowerWarning) {
+        this.groupSizes = groupSizes;
+        this.variances = variances;
+        this.isNormal = isNormal;
+        this.recommendations = recommendations;
+        this.tooFewDataPoints = tooFewDataPoints;
+        this.lowPowerWarning = lowPowerWarning;
+    }
+
+    public Map<String, Integer> getGroupSizes() {
+        return groupSizes;
+    }
+
+    public Map<String, Double> getVariances() {
+        return variances;
+    }
+
+    public Map<String, Boolean> getIsNormal() {
+        return isNormal;
+    }
+
+    public List<TestRecommendation> getRecommendations() {
+        return recommendations;
+    }
+
+    public boolean isTooFewDataPoints() {
+        return tooFewDataPoints;
+    }
+
+    public boolean isLowPowerWarning() {
+        return lowPowerWarning;
+    }
+}
