20723a2da8d48485e8d58094c87002a5dcc85438
Bjornsrud <ckb78@outlook.com>
2025-04-12 15:43:30 +0200

    Modify projectData.java to allow for more than two groups. Add default title.

--- src/main/java/com/simplesignificance/model/ProjectData.java
+++ src/main/java/com/simplesignificance/model/ProjectData.java
@@ -1,26 +1,25 @@
 package com.simplesignificance.model;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProjectData {
 
-    private String projectTitle;
-    private String group1Label;
-    private String group2Label;
+    private String projectTitle = "Untitled project";
 
-    private List<Double> group1Values;
-    private List<Double> group2Values;
+    private Map<String, List<Double>> groupData = new LinkedHashMap<>();
 
     private LocalDateTime uploadedAt = LocalDateTime.now();
+
+    private TestType selectedTestType;
 
     // TODO: Add field to associate this with a user (e.g., userId or User object)
     // private Long userId;
 
     // TODO: Add test result field when statistical test has been run
     // private TestResult result;
-
-    private TestType selectedTestType;
 
     // TODO: Add field to track whether parsing/validation succeeded
     // private boolean isValid;
@@ -33,36 +32,12 @@ public class ProjectData {
         this.projectTitle = projectTitle;
     }
 
-    public String getGroup1Label() {
-        return group1Label;
+    public Map<String, List<Double>> getGroupData() {
+        return groupData;
     }
 
-    public void setGroup1Label(String group1Label) {
-        this.group1Label = group1Label;
-    }
-
-    public String getGroup2Label() {
-        return group2Label;
-    }
-
-    public void setGroup2Label(String group2Label) {
-        this.group2Label = group2Label;
-    }
-
-    public List<Double> getGroup1Values() {
-        return group1Values;
-    }
-
-    public void setGroup1Values(List<Double> group1Values) {
-        this.group1Values = group1Values;
-    }
-
-    public List<Double> getGroup2Values() {
-        return group2Values;
-    }
-
-    public void setGroup2Values(List<Double> group2Values) {
-        this.group2Values = group2Values;
+    public void setGroupData(Map<String, List<Double>> groupData) {
+        this.groupData = groupData;
     }
 
     public LocalDateTime getUploadedAt() {
