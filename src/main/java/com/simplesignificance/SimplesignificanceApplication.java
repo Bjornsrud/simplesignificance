5fde77cf55ec3e083e4b2f0af9b509c812534225
Bjornsrud <ckb78@outlook.com>
2025-04-12 12:04:49 +0200

    Initial commit

---
+++ src/main/java/com/simplesignificance/SimplesignificanceApplication.java
@@ -0,0 +1,13 @@
+package com.simplesignificance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesignificanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesignificanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplesignificance/SimplesignificanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplesignificance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplesignificanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
