1225b3cbba4545d17358ef5b58fc404d0fd48895
Bjornsrud <ckb78@outlook.com>
2025-04-13 17:25:24 +0200

    Convert TestRecommendation.java to record class.

--- src/main/java/com/simplesignificance/model/analysis/TestRecommendation.java
+++ src/main/java/com/simplesignificance/model/analysis/TestRecommendation.java
@@ -2,26 +2,5 @@ package com.simplesignificance.model.analysis;
 
 import com.simplesignificance.model.TestType;
 
-public class TestRecommendation {
-    private TestType testType;
-    private boolean recommended;
-    private String reason;
-
-    public TestRecommendation(TestType testType, boolean recommended, String reason) {
-        this.testType = testType;
-        this.recommended = recommended;
-        this.reason = reason;
-    }
-
-    public TestType getTestType() {
-        return testType;
-    }
-
-    public boolean isRecommended() {
-        return recommended;
-    }
-
-    public String getReason() {
-        return reason;
-    }
+public record TestRecommendation(TestType testType, boolean recommended, String reason) {
 }
