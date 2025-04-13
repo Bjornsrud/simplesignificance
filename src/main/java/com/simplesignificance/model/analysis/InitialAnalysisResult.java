9dcd72f8ca0c9b388f0af6e438873cd14657e51a
Bjornsrud <ckb78@outlook.com>
2025-04-13 22:09:31 +0200

    Change orientation of parameters to improve readability, add skewness parameter.

--- src/main/java/com/simplesignificance/model/analysis/InitialAnalysisResult.java
+++ src/main/java/com/simplesignificance/model/analysis/InitialAnalysisResult.java
@@ -3,8 +3,13 @@ package com.simplesignificance.model.analysis;
 import java.util.List;
 import java.util.Map;
 
-public record InitialAnalysisResult(Map<String, Integer> groupSizes, Map<String, Double> variances,
-                                    Map<String, Boolean> isNormal, List<TestRecommendation> recommendations,
-                                    boolean tooFewDataPoints, boolean lowPowerWarning) {
-
+public record InitialAnalysisResult(
+        Map<String, Integer> groupSizes,
+        Map<String, Double> variances,
+        Map<String, Boolean> isNormal,
+        List<TestRecommendation> recommendations,
+        boolean tooFewDataPoints,
+        boolean lowPowerWarning,
+        Map<String, Double> skewness
+) {
 }
